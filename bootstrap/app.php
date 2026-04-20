030c79ffea546ef176dcc177b95561c946b9f9da
Carlos Guaderrama <carlosed1995@gmail.com>
2026-04-19 21:55:09 -0600

    Added https in workking in web

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust reverse proxies (Easypanel / ingress) so Laravel detects HTTPS correctly.
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
